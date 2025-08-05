32eb073d200544f250f8daedf9e790bc915426ad
leekleak <leekleak@proton.me>
2025-08-05 16:53:01 +0300

    update target

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,14 +6,14 @@ plugins {
 
 android {
     namespace = "com.leekleak.kvaesitsometeolt"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.leekleak.kvaesitsometeolt"
         minSdk = 26
-        targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        targetSdk = 36
+        versionCode = 3
+        versionName = "1.0.2"
         signingConfig = signingConfigs.getByName("debug")
     }
 
